696663c6c121c5455df51b96e3aa5bf30e5d2a28
jafasa <m09144192012@gmail.com>
2025-11-17 14:00:08 +0330

    Refactor project structure and update configuration files
    - Deleted unnecessary object files and dependency files from the build directory.
    - Updated project checksum to reflect recent changes.
    - Modified include paths in `c_cpp_properties.json` to point to the new project directory.
    - Adjusted executable paths in `launch.json` for debugging configurations.
    - Enhanced README.md with important notes regarding OTA configuration and usage instructions.

--- .pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/ARK_PC/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/ARK_PC/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
--- .pio/build/esp32dev/lib2bf/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/lib2bf/WebServer/detail/mimetable.cpp.o: \
- C:/Users/ARK_PC/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/ARK_PC/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/ARK_PC/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
